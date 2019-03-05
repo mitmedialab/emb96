a0469a7fb53cd6ee7c257f668ea72e56a4af11c4
yliess86 <hatiyliess86@gmail.com>
2019-03-05 12:08:18 -0500

    Add resulting midi plot image for test

--- src/exploration/latent.py
+++ src/exploration/latent.py
@@ -6,7 +6,9 @@ from tqdm import tqdm
 import matplotlib.pyplot as plt
 import numpy as np
 import torch
+import PIL
 import os
+import io
 
 def generate(output_dir, latent_size, momentum, checkpoint, n):
     decoder = Decoder((MEASURES, NOTES, SAMPLE_PER_MEASURE), latent_size, momentum)
@@ -21,7 +23,7 @@ def generate(output_dir, latent_size, momentum, checkpoint, n):
     y = y.detach().cpu().numpy()
     y = [np.hstack(y[i]) for i in range(n)]
 
-    fig  = plt.figure(figsize=(10, 8))
+    fig  = plt.figure(figsize=(10 * 4, n * 4))
     axes = [fig.add_subplot(n, 1, i + 1) for i in range(n)]
 
     for i in tqdm(range(n), total=n, desc='Ploting generated pianorolls'):
@@ -29,9 +31,15 @@ def generate(output_dir, latent_size, momentum, checkpoint, n):
         axes[i].imshow(y[i])
         axes[i].axis('off')
 
-    plt.tight_layout()
+    fig.subplots_adjust(left=0.02, right=0.98, bottom=0.02, top=0.98, wspace=0., hspace=0.02)
     fig.canvas.draw()
-    plt.show()
+
+    buf = io.BytesIO()
+    fig.savefig(buf, format='jpeg')
+    buf.seek(0)
+
+    data = PIL.Image.open(buf)
+    data.save(os.path.join(output_dir, 'midis.png'))
 
     if not os.path.isdir(output_dir):
         os.mkdir(output_dir)
